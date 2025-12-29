0934b9c2203d371096cd5784afd59312fc6afe1b
martabgr <martabagricevic@gmail.com>
2025-12-29 12:31:42 +0300

    главная + авторизрег

--- settings/routes.js
+++ settings/routes.js
@@ -1,8 +1,10 @@
 'use strict';
-const usersController = require('./../Controller/UsersController'); // Убедитесь, что путь корректный.
+const usersController = require('./../Controller/UsersController'); 
 
 module.exports = (app) => {
-    app.post('/api/auth/signup', usersController.signup); // Убедитесь, что usersController.signup - это функция
-    app.post('/api/auth/signin', usersController.signin); // То же для signin
+    app.post('/api/auth/signup', usersController.signup); 
+    app.post('/api/auth/signin', usersController.signin); 
     app.post('/api/auth/createApplication', usersController.createApplication);
+    app.get('/api/comments', usersController.getComments);
+    app.post('/api/comments', usersController.addComment);
 };
